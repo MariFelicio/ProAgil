9a8135b33b6c5fe04ec05ba0cbc92d98ad495d73
MariFelicio <marifelicio_mf@hotmail.com>
2021-01-04 10:32:28 -0300

    Salvando

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -2,6 +2,7 @@ import { EventoService } from './../_services/evento.service';
 import { Component, OnInit, TemplateRef } from '@angular/core';
 import { Evento } from '../_models/Evento';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-eventos',
@@ -10,40 +11,105 @@ import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 })
 export class EventosComponent implements OnInit {
 
+  titulo = 'Eventos';
+  dataEvento!: string;
   eventosFiltrados!: Evento[];
   eventos!: Evento[];
+  evento!: Evento;
+  modoSalvar = 'post';
   imagemLargura = 50;
   imagemMargem = 2;
-  modalRef!: BsModalRef;
+  registerForm!: FormGroup;
 
   constructor(
     private eventoService: EventoService,
-    private modalService: BsModalService
+    private modalService: BsModalService,
+    private fb: FormBuilder
     ) { }
 
   _filtroLista!: string;
-  get filtroLista(){
+  get filtroLista(): string {
     return this._filtroLista;
   }
-  set filtroLista(value: string){
+  set filtroLista(value: string) {
     this._filtroLista = value;
     this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
   }
 
-  openModal(template: TemplateRef<any>){
-    this.modalRef = this.modalService.show(template);
+  editarEvento(evento: Evento, template: any){
+    this.modoSalvar = 'put';
+    this.openModal(template);
+    this.evento = Object.assign({}, evento);
+    this.evento = evento;
+    this.registerForm.patchValue(evento);
+ }
+
+  novoEvento(template: any) {
+    this.modoSalvar = 'post';
+    this.openModal(template);
   }
 
-  ngOnInit(): void {
+  openModal(template: any) {
+    this.registerForm.reset();
+    template.show();
+  }
+
+  ngOnInit() {
+    this.validation();
     this.getEventos();
   }
 
   filtrarEventos(filtrarPor: string): Evento[] {
     filtrarPor = filtrarPor.toLocaleLowerCase();
     return this.eventos.filter(
-      (evento: { tema: string; }) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+      evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
     );
   }
+
+  validation() {
+    this.registerForm = this.fb.group({
+      tema: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
+      local: ['', Validators.required],
+      dataEvento: ['', Validators.required],
+      imagemURL: ['', Validators.required],
+      qtdPessoas: ['', [Validators.required, Validators.max(120000)]],
+      telefone: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+
+
+
+  salvarAlteracao(template: any) {
+    if (this.registerForm.valid) {
+      if (this.modoSalvar === 'post') {
+        this.evento = Object.assign({}, this.registerForm.value);
+        console.log(this.evento);
+        this.eventoService.postEvento(this.evento).subscribe(
+          () => {
+            template.hide();
+            this.getEventos();
+          }, error => {
+            console.log(error);
+          }
+        );
+      } else {
+        this.evento = Object.assign({ id: this.evento.id }, this.registerForm.value);
+        this.eventoService.putEvento(this.evento).subscribe(
+          () => {
+            template.hide();
+            this.getEventos();
+          }, error => {
+            console.log(error);
+          }
+        );
+      }
+
+    }
+  }
+
+
   getEventos(){
     this.eventoService.getAllEvento().subscribe(
       (_eventos: Evento[]) => {
